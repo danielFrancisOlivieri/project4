e0cd47d98cbcfcf7c912acd0af94a4a5a1ea55ac
Daniel Olivieri <dfolivieri@gmail.com>
2017-04-03 23:08:00 -0400

    all cleaned up

--- chat3.js
+++ chat3.js
@@ -1,174 +1,135 @@
+var XMLHttp, getCommentListBusy = false,
+    toPreventRepitition = "";
 
-     var XMLHttp, getCommentListBusy = false, toPreventRepitition = "";
-
-
-	 function init() {
-    if(navigator.appName == "Microsoft Internet Explorer") {
-	XMLHttp = new ActiveXObject("Microsoft.XMLHTTP");
+// intializes the site 
+function init() {
+    if (navigator.appName == "Microsoft Internet Explorer") {
+        XMLHttp = new ActiveXObject("Microsoft.XMLHTTP");
     } else {
-	XMLHttp = new XMLHttpRequest();
+        XMLHttp = new XMLHttpRequest();
     }
 }
+// gets comments from the server 
+function getCommentList() {
 
-$("#usermsg").keypress(function(e) {
-    if(e.which == 13) {
-       alert("What kills you in this life will be your mother in the next.");
+    if (!getCommentListBusy) {
+
+        getCommentListBusy = true;
+        var commentClass = "comment";
+        var message = "***$request$END"; // message request
+
+        XMLHttp.open("POST", "/cgi-bin/olivierid_JAD_chatAjax.cgi?" // sends the post request 
+            +
+            "message=" + message, true);
+
+        XMLHttp.onreadystatechange = function() {
+            if (XMLHttp.readyState == 4) { // this determines whether the XMLHttp better
+
+                var fromCGI = XMLHttp.responseText;
+
+                // replaces certain words 
+                fromCGI = fromCGI.replace("$END", "");
+                fromCGI = fromCGI.replace("Daniel", "my hero Daniel");
+                fromCGI = fromCGI.replace("fuck", "f***");
+                fromCGI = fromCGI.replace("Juvi", "the programmer formerly know as Juvi");
+                fromCGI = fromCGI.replace("Aidan", "Alexander Hamilton");
+
+
+                if ((toPreventRepitition != fromCGI) && ("$END" != fromCGI)) {
+                    console.log(toPreventRepitition + "  fromcgi: " + fromCGI);
+                    $("#chatbox").append(fromCGI); //adds to the chatbox
+
+                    // there was a bug when blank messages ruined it, this prevents it 
+                    if (fromCGI.length > 2) // makes sure it's a real message 
+                    {
+                        toPreventRepitition = fromCGI;
+                    }
+                }
+
+                getCommentListBusy = false; // sets it back to free and open 
+            }
+        }
+        XMLHttp.send(null);
+
     }
-});
 
-
-function getCommentList () {
-
- if (!getCommentListBusy)
- {
-
- getCommentListBusy = true;
- var commentClass = "comment";
-   var message = "***$request$END"; 
-
-    XMLHttp.open("POST", "/cgi-bin/olivierid_JAD_chatAjax.cgi?"
-						 + "message=" + message
-		 ,true);
-		 
-		 /*
-		 the next step is to go through this very carefully and try to make it as simple and functional as the other one
-		 The issue is in the XMLHttp
-		  
-		 */
-    XMLHttp.onreadystatechange=function() {
-	if (XMLHttp.readyState == 4) {
-	
-		       var fromCGI = XMLHttp.responseText;
-			  			   
-
-	fromCGI = fromCGI.replace("$END", "");
-	fromCGI = fromCGI.replace("Daniel", "my hero Daniel");
-	fromCGI = fromCGI.replace("fuck", "f***");
-	fromCGI = fromCGI.replace("Juvi", "the programmer formerly know as Juvi");
-	fromCGI = fromCGI.replace("Aidan", "Alexander Hamilton");
-	//$('.comment').remove();
-	
-	if ((toPreventRepitition != fromCGI) && ("$END" != fromCGI))
-	{
-	console.log (toPreventRepitition + "  fromcgi: " + fromCGI);
-	$("#chatbox").append(fromCGI);   //adds to the chatbox
-		if (fromCGI.length > 2)
-		{
-			toPreventRepitition = fromCGI;
-		}
-	
-	}
-	
-	getCommentListBusy = false;
-	}
-    }
-    XMLHttp.send(null);
-	
- }
-  
 }
 
 var intVar;
+// runs the commentlist function 
 function AutoRefresh() {
 
 
-    intVar = setInterval(function(){ getCommentList()}, 100);
+    intVar = setInterval(function() {
+        getCommentList()
+    }, 100);
 }
 
 function StopRefresh() {
     clearInterval(intVar);
 }
 
+function getUsername() // gets the username from the input and puts it into the welcome 
+{
 
-
-
-     function getUsername()
-     {
-
-     var name= document.getElementById('userName').value;
-     document.getElementById('user').innerHTML = name;
-commentObject.username = name;
-	$(".welcome").css("visibility", "visible");
-      }
+    var name = document.getElementById('userName').value;
+    document.getElementById('user').innerHTML = name;
+    commentObject.username = name;
+    $(".welcome").css("visibility", "visible"); // sets it to visible 
+}
 
 
 
 // comment object
 var commentObject = {
-username : "Hamlet",
-text: "",
-color: "black",
-returnOutputString: function() {
- return "$USERNAME*" + this.username + "Cato$COLOR*" + this.color + "$PICTURE*three$TEXT*" + this.text + "$END";
-}
+    username: "Hamlet",
+    text: "",
+    color: "black",
+    returnOutputString: function() {
+        return "$USERNAME*" + this.username + "Cato$COLOR*" + this.color + "$PICTURE*three$TEXT*" + this.text + "$END";
+    }
 
 }
 
 var XMLHttp, avoidReplication = "";
-if(navigator.appName == "Microsoft Internet Explorer") {
+if (navigator.appName == "Microsoft Internet Explorer") {
     XMLHttp = new ActiveXObject("Microsoft.XMLHTTP");
 } else {
     XMLHttp = new XMLHttpRequest();
 }
 
+// sends comments 
+function sendComment() {
 
-function sendComment () {
-
- var message = document.getElementById('usermsg').value;
+    var message = document.getElementById('usermsg').value;
 
 
-if (message.length > 1)
-{
+    if (message.length > 1) {
 
-  // if (name.length < 1) return;
+        commentObject.text = message;
+        message = commentObject.username + ": " + message + "$END";
 
-  commentObject.text = message;
-  message = commentObject.username + ": " + message + "$END";
-  //inMessage = commentObject.returnOutputString();
+        XMLHttp.open("GET", "/cgi-bin/olivierid_JAD_chatAjax.cgi?" +
+            "message=" + message, true);
 
-    XMLHttp.open("GET", "/cgi-bin/olivierid_JAD_chatAjax.cgi?"
-						 + "message=" + message
-						 ,true);
-						
+        XMLHttp.send(null);
 
-	XMLHttp.send(null);
-
-}
+    }
 }
 
-$(document).ready(function(){
-    $("#exit").click(function(){
-     document.getElementById('userName').disabled = false;
-	 alert("Do you want to Log Out? This will delete the chats.");
-	  $(".comment").hide();
-	  $("input").prop('disabled', true);
+// this allows the user log out 
+$(document).ready(function() {
+    $("#exit").click(function() {
+        document.getElementById('userName').disabled = false;
+        alert("Do you want to Log Out? This will delete the chats.");
+        $(".comment").hide();
+        $("input").prop('disabled', true);
     });
-  
+
 });
 
-// function logOut()
-				// {
-				 
-				 // document.getElementById('userName').disabled = false;
-				 
-				 // alert("Do you want to Log Out? This will delete the chats.");
-				 
-				 // /*if(confirm("Do you want to Log Out? This will delete the chats."))
-   				 // document.forms[0].submit();
- 				 // else
- 				   // return false; */
- 				   
-				 // var div = document.getElementById('chatbox');
-				 // div.style.visibility= 'hidden';
-				 
-				// }
-  
 
-    
-		       
-
-		       
-			   
-$( document ).ready(function() {
+// readies autorefresh 
+$(document).ready(function() {
     AutoRefresh();
 });
